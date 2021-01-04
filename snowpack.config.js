d6269419ca10b4b907113a5814ba61ec6912ab72
Adam Wirth <adamowirth@gmail.com>
2021-01-03 20:27:35 -0500

    night time ideas added to the todo
    and just reorganizing it

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,8 +1,8 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: [
     '@snowpack/plugin-react-refresh',
@@ -23,7 +23,7 @@ module.exports = {
     /* ... */
   },
   devOptions: {
-    output: 'stream', // default: "dashboard"
+    /* ... */
   },
   buildOptions: {
     /* ... */
