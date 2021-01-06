93a3b32872b629bf49edb5c35f23649cc6e5c631
Adam Wirth <adamowirth@gmail.com>
2021-01-05 19:03:12 -0500

    add some tests in
    couldn't figure out how to use .clear() with afterEach so its a more expensive render call each time instead

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { expect } from 'chai';
 import App from './App';
 
 describe('<App>', () => {
-  it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(document.body.contains(linkElement));
+  let userInput: HTMLElement;
+  let output: HTMLElement;
+  beforeEach(() => {
+    render(<App />);
+    userInput = screen.getByLabelText(/in/i);
+    output = screen.getByLabelText(/out/i);
   });
+
+  for (let i = 0; i < 10; i++) {
+    it(`has text render on numerical input ${i}`, () => {
+      const text = i.toString();
+      userEvent.type(userInput, text);
+      expect(output.innerHTML).to.be.a('string').of.length.gt(1);
+    });
+  }
 });
