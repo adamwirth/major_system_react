90e68709024205c089a3f67abfd4f603d2eaf39d
Adam Wirth <adamowirth@gmail.com>
2021-01-05 19:03:51 -0500

    linting tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
 
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
