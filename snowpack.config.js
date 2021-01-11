f89dbb557a8052769510d9927d95674046779a14
Adam Wirth <adamowirth@gmail.com>
2021-01-11 18:52:42 -0500

    adding in terser for some level of minification I don't want to spend time figuring out why snowpack with its webpack plugin wasn't working, so this'll suffice for the scope/intent of this project, I think. though knowing myself, I'll probably try and learn rollup or some third thing to migrate to at some point... updated the package.json accordingly. and added dist to .gitignore

--- snowpack.config.js
+++ snowpack.config.js
@@ -8,13 +8,11 @@ module.exports = {
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
+    // default settings for webpack here were unfortunately duplicating script output
+    // which was ultimately breaking the code!
     // '@snowpack/plugin-webpack',
-    // {
-    //   extendConfig: (config) => {
-    //     config.plugins.push(/* ... */);
-    //     return config;
-    //   },
-    // },
+    // using just terser instead for now. might switch to rollup or webpack if i feel up to the bother
+    'snowpack-plugin-terser',
   ],
   install: [
     /* ... */
